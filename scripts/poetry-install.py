3dda22b4ab13680b221ee123371ba33a1953e7d9
David Revay <daverevay@gmail.com>
2022-07-18 10:16:29 +1000

    chore: lint fixes

--- scripts/poetry-install.py
+++ scripts/poetry-install.py
@@ -1,27 +1,19 @@
 # This will run poetry install alongside any pyproject.tomls
 
+import subprocess
 from glob import glob
 from pathlib import Path
-import subprocess
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+from bcolors import bcolors
 
+if __name__ == "__main__":
+    print(bcolors.OKBLUE + "Running poetry install..." + bcolors.ENDC)
+    PACKAGES_DIRECTORY = "packages"
+    pyproject_tomls = glob(str(Path.cwd() / f"{PACKAGES_DIRECTORY}/**/pyproject.toml"))
+    pyproject_toml_dirs = [Path(item).parent for item in pyproject_tomls]
 
-PACKAGES_DIRECTORY = "packages"
-pyproject_tomls = glob(str(Path.cwd() / f"{PACKAGES_DIRECTORY}/**/pyproject.toml"))
-pyproject_toml_dirs = [Path(item).parent for item in pyproject_tomls]
+    for dir in pyproject_toml_dirs:
+        print(bcolors.OKCYAN + f"Poetry install {str(dir)}..." + bcolors.ENDC)
+        subprocess.call(f"cd {dir} && poetry install", shell=True)
 
-
-for dir in pyproject_toml_dirs:
-    print(bcolors.OKCYAN + f"Poetry install {str(dir)}..." + bcolors.ENDC)
-    subprocess.call(f'cd {dir} && poetry install', shell=True)
-
+    print(bcolors.OKGREEN + "Complete" + bcolors.ENDC)
--- scripts/test-non-ros-poetry-packages.py
+++ scripts/test-non-ros-poetry-packages.py
@@ -1,21 +1,11 @@
 # This will run pytest for any non-ros poetry packages.
 # i.e. anywhere it finds a pyproject.toml but no package.xml
 
+import subprocess
 from glob import glob
 from pathlib import Path
-import subprocess
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
+from bcolors import bcolors
 
 PACKAGES_DIRECTORY = "packages"
 package_xmls = glob(str(Path.cwd() / f"{PACKAGES_DIRECTORY}/**/package.xml"))
@@ -23,14 +13,16 @@ package_xml_dirs = [Path(item).parent for item in package_xmls]
 pyproject_tomls = glob(str(Path.cwd() / f"{PACKAGES_DIRECTORY}/**/pyproject.toml"))
 pyproject_toml_dirs = [Path(item).parent for item in pyproject_tomls]
 
-# Find the packages which are not ros packages...
-non_ros_poetry_packages = []
-for dir in pyproject_toml_dirs:
-    if (dir not in package_xml_dirs):
-        non_ros_poetry_packages.append(dir)
+if __name__ == "__main__":
+    print(bcolors.OKBLUE + "Testing non-ros poetry packages..." + bcolors.ENDC)
+    # Find the packages which are not ros packages...
+    non_ros_poetry_packages = []
+    for dir in pyproject_toml_dirs:
+        if dir not in package_xml_dirs:
+            non_ros_poetry_packages.append(dir)
 
-for dir in non_ros_poetry_packages:
-    print(bcolors.OKCYAN + f"Running tests for {str(dir)}..." + bcolors.ENDC)
-    subprocess.call(f'cd {dir} && python3 -m pytest', shell=True)
+    for dir in non_ros_poetry_packages:
+        print(bcolors.OKCYAN + f"Running tests for {str(dir)}..." + bcolors.ENDC)
+        subprocess.call(f"cd {dir} && python3 -m pytest", shell=True)
 
-
+    print(bcolors.OKGREEN + "Complete" + bcolors.ENDC)
